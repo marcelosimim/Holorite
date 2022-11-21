599ecfaac2c90c43f50d26196abfa76f7326e085
Marcelo <marcelosimim@gmail.com>
2022-11-21 16:01:49 -0300

    double extension

---
+++ Holorite/Extensions/Double.swift
@@ -0,0 +1,18 @@
+//
+//  Double.swift
+//  Holorite
+//
+//  Created by Marcelo Simim Santos on 11/21/22.
+//
+
+import Foundation
+
+extension Double {
+    func isGreaterThanOrEqualTo(_ number: Double) -> Bool {
+        self >= number
+    }
+
+    func isGreater(than number: Double) -> Bool {
+        self > number
+    }
+}
